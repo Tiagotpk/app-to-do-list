c4ecb8df0a9c1fcfe15508e3fdacccf7bee6eb70
tiagotpk <ttrpk@hotmail.com>
2023-07-19 13:35:40 -0300

    Deploy GitHub via pasta docs

---
+++ src/app/modules/home/components/to-do-button-delete-all/to-do-button-delete-all.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-button-delete-all',
+  templateUrl: './to-do-button-delete-all.component.html',
+  styleUrls: ['./to-do-button-delete-all.component.scss']
+})
+export class ToDoButtonDeleteAllComponent implements OnInit {
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
+
+}
